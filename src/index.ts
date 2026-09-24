744304b2ea9a58f3ecc41c52cdc97e07ba3ad859
stosic-dev <stefantosic04@gmail.com>
2026-09-24 13:31:26 +0200

    koristi crypto.randomUUID umesto uuid

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-// import {v4 as uuidV4} from 'uuid';
-
-// console.log(uuidV4());
+// crypto.randomUUID() je ugradjen u browser - nije potrebna uuid biblioteka
 
 const list = document.querySelector<HTMLUListElement>('#list')
-const form = document.querySelector<HTMLFormElement>('#new-task-form')
+const form = document.querySelector('#new-task-form') as HTMLFormElement | null
 const input = document.querySelector<HTMLInputElement>('#new-task-title')
+
+form?.addEventListener("submit", e => {
+  e.preventDefault()
+
+  if(input?.value == "" || input?.value == null) return
+
+  const task = {
+    id: crypto.randomUUID(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
+})
