f6d017c8783b628ce89705e0d4ba2d5739ad70a4
stosic-dev <stefantosic04@gmail.com>
2026-09-24 13:56:51 +0200

    completed toDo

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,59 @@
 // crypto.randomUUID() je ugradjen u browser - nije potrebna uuid biblioteka
 
+type Task = { 
+  id: string, 
+  title: string, 
+  completed: boolean, 
+  createdAt: Date 
+}
+
 const list = document.querySelector<HTMLUListElement>('#list')
 const form = document.querySelector('#new-task-form') as HTMLFormElement | null
 const input = document.querySelector<HTMLInputElement>('#new-task-title')
+
+const tasks: Task[] = loadTasks()
+tasks.forEach(addListItem)
 
 form?.addEventListener("submit", e => {
   e.preventDefault()
 
   if(input?.value == "" || input?.value == null) return
 
-  const task = {
+  const newTask: Task = {
     id: crypto.randomUUID(),
     title: input.value,
     completed: false,
-    createdAt: new Date()
+    createdAt: new Date(),
   }
+
+  tasks.push(newTask)
+  saveTasks()
+
+  addListItem(newTask)
+  input.value = ""
 })
+
+function addListItem(task: Task) {
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+    saveTasks()
+  })
+  checkbox.type = "checkbox"
+  checkbox.checked = task.completed
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item)
+}
+
+function saveTasks() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks))
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem("TASKS")
+  if(taskJSON == null) return []
+  return JSON.parse(taskJSON)
+}
